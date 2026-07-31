9e9bb4d15e382f5dec6df61bd5fad1781bceeede
Karthik-854321 <seelamdk968@gmail.com>
2026-07-31 14:55:35 +0530

    Add time-based greeting and focus text

--- script.js
+++ script.js
@@ -16,3 +16,28 @@ function updateClock() {
 
 updateClock();
 setInterval(updateClock, 1000);
+
+function updateGreeting() {
+  const now = new Date();
+  const hour = now.getHours();
+  let greeting;
+
+  if (hour < 5) greeting = "Good night, Karthik";
+  else if (hour < 12) greeting = "Good morning, Karthik";
+  else if (hour < 18) greeting = "Good afternoon, Karthik";
+  else greeting = "Good evening, Karthik";
+
+  const greetingEl = document.getElementById("greeting");
+  if (greetingEl) greetingEl.textContent = greeting;
+
+  const focusEl = document.getElementById("focus");
+  if (focusEl) focusEl.textContent = "Time to code and earn stardust.";
+}
+
+function updateClockAndGreeting() {
+  updateClock();
+  updateGreeting();
+}
+
+updateClockAndGreeting();
+setInterval(updateClockAndGreeting, 60000); // update every minute
